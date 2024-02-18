f5f29977ec87d46ac6b285a426e2d5339c8dcf8b
unknown <floresangela1414@gmail.com>
2024-02-18 19:06:57 +0800

    Created a simple routing and controller activity with 3 buttons with short content

---
+++ resources/views/aboutme.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ url('/aboutme')}}"> About Me</a>
+    <a href="{{ url('/skills')}}"> Skills</a>
+    <a href="{{ url('/hobbies')}}"> Hobbies</a>
+   
+    <h1>ABOUT ME</h1>
+    <p>Name: Nadia Angela D. Flores</p>
+    <p>Age:22</p>
+    <p>Height: 5'0</p>
+    <p>Facebook: Nadia Angela Flores</p>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/aboutme',[AboutMeController::class,'index']);
+Route::get('/skills',[SkillsController::class,'index']);
+Route::get('/hobbies',[HobbiesController::class,'index']);
